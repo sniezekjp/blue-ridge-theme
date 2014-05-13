d8f808f8f2a1bbcada9e6c13cb51bd31ff94298e
jp <sniezekjp@aol.com>
2014-05-13 11:03:19 -0400

    testing

--- lib/numbers-display.php
+++ lib/numbers-display.php
@@ -32,7 +32,7 @@ function export_players(){
       echo get_post_meta($id, 'guardian_phone', true) . ',';
       echo get_post_meta($id, 'guardian_email', true) . ',';
       $info = get_post_meta($id, 'medical_info', true);
-      $info = str_replace("\n", " ", $info);
+      //$info = str_replace("\n", " ", $info);
       echo '"' . $info . '",'; 
  
       //new line
