5c56c0794d4012f1adcf27888c94a2013727cf8b
jp <sniezekjp@aol.com>
2014-05-13 09:50:57 -0400

    testing

--- lib/numbers-display.php
+++ lib/numbers-display.php
@@ -1,6 +1,24 @@
 <?php
 
 //Camp numbers submenu display
-function display_camp_numbers(){
-  echo "Camp numbers";
+function display_camp_numbers(){ 
+  
+  echo '<pre>';
+  $terms = get_terms(array('camp'));
+  
+  var_dump($terms);
+
+  $players = get_posts(array(
+      'posts_per_page' => -1, 
+      'post_type' => 'player'
+    ));
+
+  var_dump($players);
+?>
+
+<h1>Camp Numbers</h1>
+
+
+
+<?php
 }
