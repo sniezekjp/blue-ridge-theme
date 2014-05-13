6048acd0f471e7305d20f80b616ccf3f5bc45f93
jp <sniezekjp@aol.com>
2014-05-13 09:54:00 -0400

    testing

--- lib/numbers-display.php
+++ lib/numbers-display.php
@@ -4,16 +4,16 @@
 function display_camp_numbers(){ 
   
   echo '<pre>';
-  $terms = get_terms(array('camp'));
-  
-  var_dump($terms);
-
+  $camps = get_terms(array('camp'));
+  $grades = get_terms(array('grade'));
   $players = get_posts(array(
       'posts_per_page' => -1, 
       'post_type' => 'player',
       'camp'=> $terms[0]->slug
     ));
 
+  var_dump($camps);
+  var_dump($grades);
   var_dump($players);
 ?>
 
